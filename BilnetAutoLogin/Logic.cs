9a98b564dc7c5b5fd927fb48ac173b18e8acabe8
mrsfy <mrsfy@outlook.com>
2016-03-14 15:28:02 +0200

    First commit

---
+++ BilnetAutoLogin/Logic.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using RestSharp;
+
+namespace BilnetAutoLogin
+{
+    public enum LoginStatus
+    {
+        INVALID_AUTH_INFO,
+        SUCCESS,
+        NO_INTERNET,
+        OUTSIDE_OF_CAMPUS_NETWORK
+    }
+    public class Logic
+    {
+
+
+        private RegistryKey registryKey;
+
+        public String Username
+        {
+            get
+            {
+                return Properties.Settings.Default.Username;
+            }
+            set
+            {
+                Properties.Settings.Default.Username = value;
+                SaveSettings();
+            }
+        }
+
+        public String Password
+        {
+            get
+            {
+                return Properties.Settings.Default.Password;
+            }
+            set
+            {
+                Properties.Settings.Default.Password = value;
+                SaveSettings();
+            }
+        }
+
+        public bool IsStartup
+        {
+            get
+            {
+                return Properties.Settings.Default.Startup;
+            }
+            set
+            {
+                Console.WriteLine(System.Environment.GetCommandLineArgs()[0]);
+                Properties.Settings.Default.Startup = value;
+                if (value)
+                {
+                    registryKey.SetValue("BilnetAutoLogin", System.Environment.GetCommandLineArgs()[0]);
+                }
+                else
+                {
+                    registryKey.DeleteValue("BilnetAutoLogin", false);
+                }
+                SaveSettings();
+            }
+        }
+
+        public Logic()
+        {
+
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(OnNetworkStatusChanged);
+            NetworkChange.NetworkAvailabilityChanged += new NetworkAvailabilityChangedEventHandler(OnNetworkStatusChanged);
+            OnNetworkStatusChanged(null, null);
+            registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        }
+
+        public void SaveSettings()
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        public void OnNetworkStatusChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Network Status Changed!");
+            //Console.WriteLine("Google ping: " + IsGoogleAvailable());
+            Console.WriteLine("html result: " + TryLogin());
+        }
+
+        
+        private LoginStatus TryLogin()
+        {
+
+
+            var client = new RestClient("https://auth2.bilkent.edu.tr") ;
+            var request = new RestRequest("auth/login", Method.POST);
+            request.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+            request.AddParameter("UserName", Username);
+            request.AddParameter("Password", Password);
+            request.AddParameter("agree", true);
+            request.ReadWriteTimeout = 1000;
+            IRestResponse response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            LoginStatus result;
+            if (response.Content.Contains("Redirect to Bilnet login page...."))
+            {
+                Console.WriteLine("Redirect to Bilnet login page....");
+                Task.Delay(3000);
+                result = TryLogin();
+            }
+            else if (response.Content.Contains("Invalid ID or Password"))
+            {
+                Console.WriteLine("Invalid ID or Password");
+                result = LoginStatus.INVALID_AUTH_INFO;
+            }
+            else if (response.Content.Contains("This service is available only on-campus."))
+            {
+                Console.WriteLine("This service is available only on-campus.");
+                result = LoginStatus.OUTSIDE_OF_CAMPUS_NETWORK;
+            }
+            else if (response.StatusCode == 0)
+            {
+                Console.WriteLine("No Internet!");
+                result = LoginStatus.NO_INTERNET;
+            }
+            else
+            {
+                result = LoginStatus.SUCCESS;
+            }
+
+            return result;
+        }
+
+
+        private bool IsGoogleAvailable()
+        {
+            bool result = false;
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                    PingReply pingReply = ping.Send("www.google.com");
+
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        result = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+            return result;
+        }
+        
+    }
+}
---
+++ BilnetAutoLogin/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BilnetAutoLogin
+{
+
+    public partial class MainWindow : MetroWindow
+    {
+        private Logic _logic;
+
+        public MainWindow()
+        {
+            _logic = new Logic();
+            InitializeComponent();
+            BindSettings();
+        }
+
+        private void BindSettings()
+        {
+             usernameText.Text = _logic.Username;
+             passwordText.Password = _logic.Password;
+             startupCheckBox.IsChecked = _logic.IsStartup;
+        }
+
+        private void Username_Changed(object sender, TextChangedEventArgs e)
+        {
+            _logic.Username = (sender as TextBox).Text;
+            successfullChanges();
+        }
+
+        private void Password_Changed(object sender, RoutedEventArgs e)
+        {
+            _logic.Password = (sender as PasswordBox).Password;
+            successfullChanges();
+
+        }
+        private void successfullChanges()
+        {
+            resultLabel.Foreground = new SolidColorBrush(Colors.Green);
+            resultLabel.Content = "Changes applied successfully.";
+        }
+
+        private void manual_button_Click(object sender, RoutedEventArgs e)
+        {
+            _logic.OnNetworkStatusChanged(sender, e);
+        }
+        private void startupCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            _logic.IsStartup = true;
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to close Bilnet Auto Login Completely?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        private void MetroWindow_StateChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Normal:
+                    break;
+                case WindowState.Minimized:
+                    this.Hide();
+                    break;
+                case WindowState.Maximized:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TaskbarIcon_TrayLeftMouseUp(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+            this.Activate();
+            this.Topmost = true;
+            this.Topmost = false;
+            this.Focus();
+        }
+
+        private void startupCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _logic.IsStartup = false;
+        }
+    }
+
+}
